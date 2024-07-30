cf6817163f3c22d4b776b527de92d5c67fc9205e
peram-vignesh <peramvigneshchowdary@gmail.com>
2024-07-30 15:18:33 +0530

    init

--- src/components/Videocarousel.jsx
+++ src/components/Videocarousel.jsx
@@ -49,17 +49,57 @@ const Videocarousel = () => {
     const handleLoadedMetadata = (i, e) => setloadedData((prev) => [...prev, e]);
 
     useEffect(() => {
-        const currentProgress = 0;
+        let currentProgress = 0;
         let span = videoSpanRef.current;
 
         if (span[videoId]) {
             // animate the progress of video
             let anim = gsap.to(span[videoId], {
-                onUpdate: () => { },
+                onUpdate: () => {
+                    const progress= Math.ceil(anim.progress()*100);
 
-                onComplete: () => { }
+                    if (progress != currentProgress){
+                        currentProgress=progress;
+
+                        gsap.to(videoDivRef.current[videoId],{
+                            width:window.innerWidth<760? '10vw'
+                            : window.innerWidth<1200 ? '10vw'
+                            : '4vw'
+                        })
+
+                        gsap.to(span[videoId],{
+                            width: `${currentProgress}%`,
+                            backgroundColor: 'white'
+                        })
+                    }
+                 },
+
+                onComplete: () => {
+                    if (isPlaying){
+                        gsap.to(videoDivRef.current[videoId],{
+                            width:'12px'
+                        })
+                        gsap.to(span[videoId],{
+                            backgroundColor:'#afafaf'
+                        })
+                    }
+                 }
             });
+
+            if (videoId === 0){
+                anim.restart();
+            }
+            const animUpdate = () => {
+                anim.progress(videoRef.current[videoId] / 
+                    highlightsSlides[videoId].videoDuration)
+            }
+
+        if(isPlaying){
+            gsap.ticker.add(animUpdate)
+        }else{
+            gsap.ticker.remove(animUpdate)
         }
+    }
     }, [videoId, startPlay]);
 
     const handleProcess = (type, i) => {
